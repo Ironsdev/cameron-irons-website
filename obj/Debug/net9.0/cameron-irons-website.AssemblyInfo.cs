9f58da6e340929a16a226d6d6828c118a44b5a96
Blake Irons <IronDev@tuta.com>
2025-12-16 00:20:35 -0600

    touchup

--- obj/Debug/net9.0/cameron-irons-website.AssemblyInfo.cs
+++ obj/Debug/net9.0/cameron-irons-website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cameron-irons-website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789d07748900ffc7eadac439c1d93e435da74d4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57266ad18f7bcdbab0a8fe4b8e1c018f7dc54768")]
 [assembly: System.Reflection.AssemblyProductAttribute("cameron-irons-website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cameron-irons-website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
